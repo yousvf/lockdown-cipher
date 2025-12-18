52c7c53f8ac2d4f16a919c9180189e164c6516e1
yousvf <145223965+yousvf@users.noreply.github.com>
2025-12-18 18:25:04 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,21 @@ import { RowColumnPage } from './pages/RowColumnPage';
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }, [pathname]);
+
+  return null;
+}
+
+export default ScrollToTop;
+
 function App() {
   const { pathname } = useLocation();
   useEffect(() => {
